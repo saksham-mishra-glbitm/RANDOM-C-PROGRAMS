27b48b2c3187808ac9fff330a4a782d83ccef489
saksham-mishra-glbitm <kenzydope37@gmail.com>
2026-02-14 22:31:55 +0530

    Kms to Miles and vice versa.
    WAP to input distance in miles and kilometers and convert them to kilometers and miles respectively.

---
+++ km2milesvv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    
+    double kms=0.0, miles=0.0;
+    printf("Enter the distance in kilometers:\n");
+    scanf("%lf",&kms);
+    printf("\nEnter the distance in miles:\n");
+    scanf("%lf",&miles);
+
+    double resmiles = 0.621371192*kms;
+    double reskms = miles/0.621371192;
+
+    printf("\n\n%lf in miles = %lf.",kms,resmiles);
+    printf("\n%f in kilometers = %lf.",kms,reskms);
+    
+    return 0;
+}
